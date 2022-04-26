2c1e70c8fe759c5620fd5b89c0c514dad7e36678
Zulaa <software>
2022-04-26 11:23:41 +0800

    davka

--- src/screens/militaryRule/JagsaalAjillagaa/JagsaaliinAjillaga.js
+++ src/screens/militaryRule/JagsaalAjillagaa/JagsaaliinAjillaga.js
@@ -1,145 +1,6 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  ScrollView,
-  Image,
-} from "react-native";
-import AppIntroSlider from "react-native-app-intro-slider";
-import { selectQuestionsByDurem } from "../../../helpers/dbMilitaryRule";
-import ShowResult from "../../../components/militaryRule/testYourself/ShowResult";
-import ShowQuestion from "../../../components/militaryRule/testYourself/ShowQuestion";
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showRealApp: false,
-    };
-  }
-
-  //   componentDidMount = () => {
-  //     console.log("asd");
-  //     selectQuestionsByDurem(this.props.route.params.bulegId)
-  //       .then((res) => {
-  //         // console.log(res._array);
-  //         this.setState({ questions: res._array });
-
-  //         let answers = [];
-  //         res._array.map((el) => {
-  //           var el1 = { id: el.id, trueAnswer: el.true_answer, userAnswer: "" };
-  //           answers.push(el1);
-  //         });
-  //         console.log(answers);
-  //         this.setState({ answers: answers });
-  //       })
-  //       .catch((err) => {});
-  //   };
-
-  _renderItem = ({ item }) => {
-    return (
-      <View style={styles.slide}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Image source={item.image} />
-        <Text style={styles.text}>{item.text}</Text>
-      </View>
-    );
-  };
-  _onDone = () => {
-    this.setState({ showRealApp: true });
-  };
-  render() {
-    if (this.state.showRealApp) {
-      return <App />;
-    } else {
-      return (
-        <>
-          <View style={styles.container}>
-            <AppIntroSlider
-              renderItem={this._renderItem}
-              data={slides}
-              onDone={this._onDone}
-            />
-          </View>
-        </>
-      );
-    }
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  backButton: {
-    backgroundColor: "#00aeef",
-    padding: 15,
-    height: 45,
-  },
-  headerRow: {
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    height: 45,
-    width: "100%",
-    backgroundColor: "green",
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 16,
-    fontWeight: "bold",
-    flex: 1,
-    color: "#fff",
-    padding: 2,
-  },
-  line: {
-    width: 1,
-    backgroundColor: "#fff",
-    marginRight: 3,
-    height: "100%",
-  },
-
-  slide: {
-    flex: 1,
-    padding: 5,
-    backgroundColor: "#fff",
-    overflow: "hidden",
-  },
-
-  quesText: {
-    color: "black",
-    fontSize: 14,
-    fontWeight: "bold",
-    borderRadius: 5,
-  },
-  ansBtn: {
-    marginBottom: 5,
-    borderRadius: 5,
-    padding: 5,
-    marginLeft: 20,
-    marginRight: 20,
-    backgroundColor: "green",
-  },
-  ansText: {
-    color: "#fff",
-    fontSize: 13,
-  },
-  questionsStyle: {
-    borderRadius: 10,
-    borderColor: "green",
-    borderWidth: 2,
-    padding: 5,
-    marginBottom: 5,
-  },
-  questionsText: {
-    color: "black",
-    fontSize: 13,
-  },
-});
-
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import Swiper from "react-native-swiper";
 const slides = [
   {
     key: "one",
@@ -163,3 +24,40 @@ const slides = [
     backgroundColor: "#22bcb5",
   },
 ];
+
+const JagsaaliinAjillaga = () => {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {}, []);
+  return (
+    <Swiper
+      style={styles.wrapper}
+      showsButtons={true}
+      loop={false}
+      showsPagination={false}
+    >
+      {slides.map((el, index) => (
+        // <ShowQuestion key={el.id} item={el} />
+        <>
+          <Text key={el.key} style={styles.titleTxt}>
+            {el.title}
+          </Text>
+          <Image style={styles.image} source={el.image} />
+        </>
+      ))}
+    </Swiper>
+  );
+};
+
+export default JagsaaliinAjillaga;
+
+const styles = StyleSheet.create({
+  titleTxt: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
